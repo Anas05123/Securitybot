34817237d45c4a8f4e1e0b4220c219192b18f839
Minecraft <44584193+anasayari@users.noreply.github.com>
2019-03-27 19:12:28 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -59,4 +59,16 @@ client.on('message', message => {
 	}
 });
 
+client.on('message', message => {
+    if(!message.channel.guild) return;
+let args = message.content.split(' ').slice(1).join(' ');
+if (message.content.startsWith(prefix + 'bc-users')){
+if(!message.author.id === 'اي دي صاحب البوت') return;
+message.channel.sendMessage('جار ارسال الرسالة |:white_check_mark:')
+client.users.forEach(m =>{
+m.sendMessage(args)
+})
+}
+});
+
 client.login("NTYwMzQ4MDEwODgyNjYyNDA1.D30OyA.BDNLTG4fwpZRwRMSxx53QZIxnoE");
