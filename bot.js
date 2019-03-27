a6f9128c0218b13b1d6fce489e53908b539da62a
Minecraft <44584193+anasayari@users.noreply.github.com>
2019-03-27 21:10:14 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -101,7 +101,7 @@ client.on("message", (message) => {
  
    if (message.content.startsWith(prefix + "new")) {  
         const reason = message.content.split(" ").slice(1).join(" ");  
-        if (!message.guild.roles.exists("name", "Helpers Team")) return message.channel.send(`لازم تسوي رتبة اسمها \`Helpers Team\` وتنطي البوت ادمنيتر حتا يقدر يسوي الرومات ويعدل برمشنات`);
+        if (!message.guild.roles.exists("name", "Helpers Team")) return message.channel.send(`لازم تسوي رتبة اسمها \`Helpers Team\` وتعطي البوت ادمنيتر حتا يقدر يسوي الرومات ويعدل برمشنات`);
         if (message.guild.channels.exists("name", "ticket-{message.author.id}" + message.author.id)) return message.channel.send(`You already have a ticket open.`);    /// ALPHA CODES
         message.guild.createChannel(`ticket-${message.author.username}`, "text").then(c => {
             let role = message.guild.roles.find("name", "Helpers Team");
