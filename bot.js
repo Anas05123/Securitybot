68b8e4d08dccede5841e444729d7679830ccd33d
Minecraft <44584193+anasayari@users.noreply.github.com>
2019-03-27 22:03:22 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,9 +1,5 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const fs = require('fs');
-const moment = require('moment');
-const jimp = require('jimp');
-const Canvas = require('canvas');
 const prefix = "$";
 
 client.on('ready',  () => {
